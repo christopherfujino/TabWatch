0413dd02bd349ed6f403a6ea3335d93010081609
Christopher Fujino <christopherfujino@gmail.com>
2017-07-09 14:44:04 -0700

    changed: badge text now updates when tabs are created, closed, attached or detached

--- dialog.js
+++ dialog.js
@@ -1,7 +1,4 @@
 chrome.tabs.query({}, function (arr) {
   let tabCount = '' + arr.length;
-  chrome.browserAction.setBadgeText({
-    text: tabCount
-  });
   document.getElementById('tabCount').innerHTML = tabCount;
 });
--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-alert('YOLODAWG!');
+function updateBadge () {
+  chrome.tabs.query({}, function (tabs) {
+    chrome.browserAction.setBadgeText({
+      text: '' + tabs.length
+    });
+  }); // query onLoad
+}
+
+updateBadge();  // onLoad
+chrome.tabs.onCreated.addListener(updateBadge);
+chrome.tabs.onRemoved.addListener(updateBadge);
+chrome.tabs.onDetached.addListener(updateBadge);
+chrome.tabs.onAttached.addListener(updateBadge);
