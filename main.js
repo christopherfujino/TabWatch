9b70a47315b8fdf688cf267c7cf5a50693eeafe1
Christopher Fujino <christopherfujino@gmail.com>
2017-07-09 16:58:58 -0700

    added: badge bgcolor now changes based on if numOfTabs is over, under, or equal to limit

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // could this be an event page rather than background?
 // https://developer.chrome.com/extensions/event_pages
+let limit = 5;
 
 const colors = {
   green: '#008744',
@@ -14,6 +15,13 @@ function updateBadge () {
     chrome.browserAction.setBadgeText({
       text: '' + tabs.length
     });
+    let color;
+    if (tabs.length < limit) color = colors.green;
+    else if (tabs.length === limit) color = colors.yellow;
+    else color = colors.red;
+    chrome.browserAction.setBadgeBackgroundColor({
+      color: color
+    })
   }); // query onLoad
 }
 
