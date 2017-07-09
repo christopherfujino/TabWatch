f8ae14cb3188de42bbe0ebc9feae2619be1fee3b
Christopher Fujino <christopherfujino@gmail.com>
2017-07-09 13:22:55 -0700

    added: basic tab counting code

---
+++ app.js
@@ -0,0 +1,8 @@
+chrome.tabs.getAllInWindow(function (arr) {
+  arr.forEach(function (tab) {
+    let span = document.getElementById('tabCount');
+    let count = parseInt(span.innerHTML, 10);
+    if (isNaN(count)) count = 0;
+    span.innerHTML = ++count;
+  });
+});
