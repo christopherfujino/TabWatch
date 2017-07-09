f7f7d69c5f5bc2866900f65db63aac5c04002424
Christopher Fujino <christopherfujino@gmail.com>
2017-07-09 14:26:15 -0700

    changed: streamlined code

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 chrome.tabs.query({}, function (arr) {
-  arr.forEach(function (tab) {
-    let span = document.getElementById('tabCount');
-    let count = parseInt(span.innerHTML, 10);
-    if (isNaN(count)) count = 0;
-    span.innerHTML = ++count;
-  });
+  let count = '' + arr.length;
+
+  chrome.browserAction.setBadgeText(count);
+  document.getElementById('tabCount').innerHTML = count;
 });
