9ce70875a3b4a2291e3368e06abd636261f22e9a
Christopher Fujino <christopherfujino@gmail.com>
2017-07-09 16:18:33 -0700

    added: code to switch panes

--- dialog.js
+++ dialog.js
@@ -1,4 +1,27 @@
+const navLinks = document.getElementById('nav-links');
+
+function getSectionFromNav (nav) {
+  return document.getElementById(nav.getAttribute('data-section'));
+}
+
+navLinks.addEventListener('click', function (e) {
+  target = e.target;
+  if (target.nodeName !== 'LI' || target.classList.contains('active')) return;
+  let lastNav = navLinks.querySelector('.active')
+  lastNav.classList.remove('active');
+  lastSection = getSectionFromNav(lastNav);
+  lastSection.classList.remove('active');
+  target.classList.add('active'); // change styling of clicked nav el
+  let nextSection = getSectionFromNav(target);
+  nextSection.classList.add('active');
+})
+
 chrome.tabs.query({}, function (arr) {
   let tabCount = '' + arr.length;
   document.getElementById('globalTabCount').innerHTML = tabCount;
+});
+
+chrome.tabs.query({currentWindow: true}, function (arr) {
+  let tabCount = '' + arr.length;
+  document.getElementById('localTabCount').innerHTML = tabCount;
 });
